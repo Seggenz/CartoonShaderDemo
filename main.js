18b9fa3ae86f998d4fb5cdf43254b1e9dd808909
Rayugo <jan.szolc@gmail.com>
2024-06-01 10:37:01 +0200

    Added control panel for choosing different model

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ import {ObjectRenderer} from './object_renderer.js';
 console.log('Creating ObjectRenderer instance');
 const objectRenderer = new ObjectRenderer();
 console.log('Calling renderObject');
-objectRenderer.renderObject('./models/monkey2.glb');
+objectRenderer.renderObject('./models/skrzynia.glb');
 console.log('Calling animate');
 objectRenderer.animate();
+
+document.getElementById('boxModelButton').addEventListener('click', () => {
+  objectRenderer.renderObject('./models/skrzynia.glb');
+});
+
+document.getElementById('claptrapModelButton').addEventListener('click', () => {
+  objectRenderer.renderObject('./models/claptrap2.glb');
+});
+
+document.getElementById('gunModelButton').addEventListener('click', () => {
+  objectRenderer.renderObject('./models/glock.glb');
+});
